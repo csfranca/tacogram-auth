6142974f266cecf51f3e4c0561bcd25a58414963
Camila Franca <camila.franca@kellogg.northwestern.edu>
2023-05-28 22:15:21 +0000

    Step 3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,24 @@
 class PostsController < ApplicationController
 
-    def index
-      @posts = Post.all
-    end
-  
-    def new
-      @post = Post.new
-    end
-  
-    def create
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    if @current_user
       @post = Post.new
       @post["body"] = params["post"]["body"]
       @post["image"] = params["post"]["image"]
+      @post["user_id"] = @current_user["id"]
       @post.save
-      redirect_to "/posts"
+    else
+      flash["notice"] = "Login first."
     end
-  
+    redirect_to "/posts"
   end
+
+end
